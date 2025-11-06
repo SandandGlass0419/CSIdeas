287b2220024ad657211c59a9f08f8dcfc25add79
SandandGlass <sandandglass0419@gmail.com>
2025-11-06 16:06:03 +0900

    ErrorHandlingIdea
    idea implementation on custom error action by package user.

---
+++ ErrorHandlingIdea/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericErrorTest;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("main start");
+        EnvironmentError.HookEvent();
+
+        GenericClass.Function();
+    }
+}
+
+public class EnvironmentError : IEnvironmentErrorHandler
+{
+    public static void HookEvent()
+    {
+        ErrorThrower.ErrorEvent += OnError;
+    }
+
+    public static void OnError(object sender, ErrorEventArgs e)
+    {
+        Console.WriteLine(e.Message);
+        Console.WriteLine("exited");
+    }
+}
